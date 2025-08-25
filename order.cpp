3c12436ac4cb898605fbb42a659241e3a36ce998
stu-account <u246165692@tuks.co.za>
2025-08-25 13:20:27 +0200

    added some state related functions, didn't do the full thing tho

--- order.cpp
+++ order.cpp
@@ -1,5 +1,13 @@
 #include "order.h"
 
+Order::Order(OrderState* initialState, DiscountStrategy* initialDiscount) {
+    //not implemented yet
+}
+
+Order::~Order() {
+    //not implemented yet
+}
+
 void Order::setDiscountStrategy(DiscountStrategy* strategy){
     this->discountStrategy = strategy;
 }
@@ -9,8 +17,34 @@ double Order::calculateSubtotal(){
     for (Pizza* pizza : pizzas) {
         subtotal+= pizza->getPrice();
     }
+    return subtotal;
 } 
 
 double Order::calculateTotal(){
     return discountStrategy->applyDiscount(calculateSubtotal());
+}
+
+void Order::addPizza(Pizza* pizza) {
+    this->pizzas.push_back(pizza);
+}
+
+void Order::setState(OrderState* newState) {
+    if (this->state) delete this->state;
+    this->state = newState;
+}
+
+void Order::confirmOrder() {
+    this->state->confirmOrder(this);
+}
+
+void Order::prepareOrder() {
+    this->state->prepareOrder(this);
+}
+
+void Order::completeOrder() {
+    this->state->completeOrder(this);
+}
+
+void Order::cancelOrder() {
+    this->state->cancelOrder(this);
 }
--- order.h
+++ order.h
@@ -1,21 +1,38 @@
 /*This will act as the context for our strategy*/
+/*Will also be the context for state*/
 #ifndef ORDER_H
 #define ORDER_H
 
 #include "pizza.h"
 #include "discountstrategy.h"
+#include "orderstate.h"
 
 #include <vector>
 
 class Order
 {
     private:
-        std::vector<Pizza*> pizzas;
+        OrderState* state; //state pattern
+        std::vector<Pizza*> pizzas; //strategy 
         DiscountStrategy* discountStrategy;
     public:
+        Order(OrderState* initialState, DiscountStrategy* initialStrategy);
+        ~Order();
+
+        void setState(OrderState* newState);
+        OrderState* getState() const;
+
+        void addPizza(Pizza* pizza);
+        void confirmOrder();
+        void prepareOrder();
+        void completeOrder();
+        void cancelOrder();
+
         void setDiscountStrategy(DiscountStrategy* strategy);
         double calculateSubtotal();
         double calculateTotal();
+
+        
 };
 
 #endif
